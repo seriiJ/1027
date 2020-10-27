22dbccd0f5a51a63070a82da62e3e211f577cc58
seriiJ <62724826+seriiJ@users.noreply.github.com>
2020-10-27 22:21:59 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,164 @@
+$(document).ready(function () {
+
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".box1").css("background-color", "#f1d42c");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".box1").css("background-color", "purple");
+        }
+    })
+
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".box3").css("background-color", "rgb(224, 0, 0)");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".box3").css("background-color", "#254EA1");
+        }
+    })
+
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".box5").css("background-color", "#254EA1");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".box5").css("background-color", "purple");
+        }
+    })
+
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".box4").css("background-color", "#254EA1");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".box4").css("background-color", "#f1d42c");
+        }
+    })
+
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".box8").css("background-color", "#f1d42c");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".box8").css("background-color", "#254EA1");
+        }
+    })
+
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".circle3").css("background-color", "rgb(224, 0, 0)");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".circle3").css("background-color", "#254EA1");
+        }
+    })
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".circle2").css("background-color", "#000000");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".circle2").css("background-color", "rgb(224, 0, 0)");
+        }
+    })
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".circle6").css("background-color", "#000000");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".circle6").css("background-color", "#2AAF5F");
+        }
+    })
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".circle7").css("background-color", "#EF8B9F");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".circle7").css("background-color", "#000000");
+        }
+    })
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".circle").css("background-color", "#f1d42c");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".circle").css("background-color", "#254EA1");
+        }
+    })
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".box7").css("background-color", "#2AAF5F");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".box7").css("background-color", "rgb(224, 0, 0)");
+        }
+    })
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".box9").css("background-color", "rgb(224, 0, 0)");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".box9").css("background-color", "#EF8B9F");
+        }
+    })
+    var windowWidth = $(window).width()
+    var mouseX = 0;
+
+    $(document).mousemove(function (e) {
+        mouseX = e.pageX;
+        if (mouseX < windowWidth / 2) {
+            $(".box10").css("background-color", "#2AAF5F");
+        }
+        if (mouseX > windowWidth / 2) {
+            $(".box10").css("background-color", "rgb(224, 0, 0)");
+        }
+    })
+})
